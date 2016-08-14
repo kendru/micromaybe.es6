309520f9b74233fd967aeaf48d37f16afb69ccd8
Andrew Meredith <kendru@users.noreply.github.com>
2016-08-13 23:06:32 -0600

    Fix export names

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const Just = x => (ok) => ok(x)
 const Nothing = () => (_, err) => err()
 const fmap = (m, f) => m(x => Just(f(x)), () => Nothing())
 const bind = (m, f) => m(x => f(x), () => Nothing())
-export { just, nothing, fmap, bind }
+export { Just, Nothing, fmap, bind }
