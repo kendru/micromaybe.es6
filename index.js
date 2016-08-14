f9a7e831c47e20199fe0eb2b7f0d817d51a2d693
Andrew Meredith <kendru@users.noreply.github.com>
2016-08-13 23:06:11 -0600

    Capitalize type constructors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const just = x => (ok) => ok(x)
-const nothing = () => (_, err) => err()
-const fmap = (m, f) => m(x => just(f(x)), () => nothing())
-const bind = (m, f) => m(x => f(x), () => nothing())
+const Just = x => (ok) => ok(x)
+const Nothing = () => (_, err) => err()
+const fmap = (m, f) => m(x => Just(f(x)), () => Nothing())
+const bind = (m, f) => m(x => f(x), () => Nothing())
 export { just, nothing, fmap, bind }
